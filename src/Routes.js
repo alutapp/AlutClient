8afac3a4e68b5d0793767a5f72a120581916b43d
chen <chenshooker@gmail.com>
2018-06-05 15:00:16 +0300

    fix merge

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Router, Stack, Scene} from 'react-native-router-flux';
-
+import Password from './pages/Password'
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Createprofile from './pages/Createprofile';
