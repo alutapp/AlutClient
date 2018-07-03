623a9d53189650de53d411de205e493475cf5234
chen <chenshooker@gmail.com>
2018-07-03 16:12:10 +0300

    images resource

---
+++ src/resources/images.js
@@ -0,0 +1,16 @@
+
+const Images = {
+    boy: require('../images/boy.png'),
+    girl: require('../images/girl.png'),
+    location: require('../images/location.png'),
+    cinema: require('../images/hobbies/cinema.png'),
+    cooking: require('../images/hobbies/cooking.png'),
+    dancing: require('../images/hobbies/dancing.png'),
+    dolls: require('../images/hobbies/dolls.png'),
+    gaming: require('../images/hobbies/gaming.png'),
+    music: require('../images/hobbies/music.png'),
+    puzzles: require('../images/hobbies/puzzles.png'),
+    soccer: require('../images/hobbies/soccer.png'),
+    theatre: require('../images/hobbies/theatre.png'),
+  };
+export default Images;  
