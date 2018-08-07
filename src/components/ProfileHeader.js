29d3386fdbc0e5df912189ff0118f3dca8911445
ziv <ziv.nachum@gmail.com>
2018-08-07 15:45:44 +0300

    fixed create profile pages

--- src/components/ProfileHeader.js
+++ src/components/ProfileHeader.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
 import {
-	StyleSheet,
-	Text,
-	View,
+    StyleSheet,
+    Text,
+    View,
     Image,
-    Props
+    Props,
+    TouchableOpacity
 } from 'react-native';
-import {Header,Icon} from 'react-native-elements';
+import { Actions } from 'react-native-router-flux';
+import { Header, Icon } from 'react-native-elements';
 
 export default class MyHeader extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.headerName = props.headerName;
     }
 
-   render(){
-		 return(
-        <Header
-						outerContainerStyles={{ backgroundColor: '#455a64' }}
-            centerComponent={{text: this.headerName, style: {fontWeight:'bold', fontSize:20}}}
-            leftComponent = {<Icon name='navigate-next' />}
-						/>
-    )};
+    render() {
+        return (
+            <Header
+                outerContainerStyles={{ backgroundColor: '#455a64' }}
+                centerComponent={{ text: this.headerName, style: { fontWeight: 'bold', fontSize: 20 } }}
+                leftComponent={
+                    <TouchableOpacity>
+                        <Icon name='navigate-next' />
+                    </TouchableOpacity>}
+            />
+        )
+    };
 }
--- src/pages/CreateProfile.js
+++ src/pages/CreateProfile.js
@@ -3,7 +3,7 @@ import {
   StyleSheet,
   Text,
   View,
-  StatusBar ,
+  StatusBar,
   TouchableOpacity
 } from 'react-native';
 
@@ -16,67 +16,68 @@ import SecondStageProfile from '../components/CreateProfile/SecondStageProfile';
 import ThirdStageProfile from '../components/CreateProfile/ThirdStageProfile';
 import FinishStageProfile from '../components/CreateProfile/FinishStageProfile';
 import ProfileHeader from '../components/ProfileHeader';
-import {Actions} from 'react-native-router-flux';
+import { Actions } from 'react-native-router-flux';
 import PageControl from 'react-native-page-control';
 
 export default class Signup extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.currentPage = 0;
-    this.state = {currentPage : 0 , numOfPages: 3};
-    this.pagesComponents = [<FirstStageProfile/>, <SecondStageProfile/>,<ThirdStageProfile/>,<FinishStageProfile/>];
+    this.state = { currentPage: 0, numOfPages: 4 };
+    this.pagesComponents = [<FirstStageProfile />, <SecondStageProfile />, <ThirdStageProfile />,
+    <FourthStageProfile />, <FinishStageProfile />];
   }
 
 
   goBack() {
-      Actions.pop();
+    Actions.pop();
   }
 
   onPress = () => {
-    if(this.state.currentPage < this.state.numOfPages){
+    if (this.state.currentPage < this.state.numOfPages) {
       this.setState({
-        currentPage: this.state.currentPage+1
+        currentPage: this.state.currentPage + 1
       })
     }
   }
 
-	render() {
-		return(
-			<View style = {globalStyles.container}>
-      <ProfileHeader headerName={strings.headerTitle}/>
-      <View style = {styles.container}>
-      <PageControl style={styles.pageControl}
-      numberOfPages={3}
-      currentPage={this.state.currentPage}
-      hidesForSinglePage
-      pageIndicatorTintColor='gray'
-      currentPageIndicatorTintColor='white'
-      indicatorStyle={{borderRadius: 5}}
-      currentIndicatorStyle={{borderRadius: 5}}
-      indicatorSize={{width:8, height:8}}
-      onPageIndicatorPress={this.onItemTap}
-      />
-
-      {this.pagesComponents[this.state.currentPage]}
-
-
-      <TouchableOpacity style={styles.button} onPress={this.onPress}>
-        <Text style={styles.buttonText}>{this.state.currentPage != this.state.numOfPages ? strings.nextPage : strings.goToApp}</Text>
-      </TouchableOpacity>
-
+  render() {
+    return (
+      <View style={globalStyles.container}>
+        <ProfileHeader headerName={strings.headerTitle} />
+        <View style={styles.container} flex={1}>
+          <PageControl style={styles.pageControl}
+            numberOfPages={4}
+            currentPage={this.state.currentPage}
+            hidesForSinglePage
+            pageIndicatorTintColor='gray'
+            currentPageIndicatorTintColor='white'
+            indicatorStyle={{ borderRadius: 5 }}
+            currentIndicatorStyle={{ borderRadius: 5 }}
+            indicatorSize={{ width: 8, height: 8 }}
+            onPageIndicatorPress={this.onItemTap}
+          />
+          <View flex={8}>
+            {this.pagesComponents[this.state.currentPage]}
+          </View>
+          <View style={{ flexDirection: 'column', paddingBottom: 5 }} flex={1}>
+            <TouchableOpacity style={globalStyles.smallButton} onPress={this.onPress}>
+              <Text style={styles.buttonText}>{this.state.currentPage != this.state.numOfPages ? strings.nextPage : strings.goToApp}</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
-			</View>
-			)
-	}
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  container : {
-    alignItems:'center'
+  container: {
+    alignItems: 'center'
   },
-  pageControl : {
-    paddingVertical:20
+  pageControl: {
+    paddingVertical: 20
   },
   button: {
     width:300,
@@ -87,11 +88,9 @@ const styles = StyleSheet.create({
 
   },
   buttonText: {
-    fontSize:16,
-    fontWeight:'500',
-    color:'#ffffff',
-    textAlign:'center',
-    paddingVertical:10
-
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#ffffff',
+    textAlign: 'center',
   }
 });
